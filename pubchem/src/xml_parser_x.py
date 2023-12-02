c532bcf3a72f3cea684ebfe2285fb2374ab9003d
Saketh <saketh9977@gmail.com>
2023-12-02 20:17:52 +0530

    pubchem: parsed all props

--- pubchem/src/xml_parser_x.py
+++ pubchem/src/xml_parser_x.py
@@ -3,7 +3,7 @@ from xml.etree import ElementTree as ET
 import os
 import json
 
-def get_iupac(node):
+def get_infodata(node, label, name=None, type_='sval'):
     if node.tag != 'PC-InfoData':
         return
     
@@ -16,52 +16,23 @@ def get_iupac(node):
     urn_label = urn.find('PC-Urn_label')
     if urn_label is None:
         return
-    if urn_label.text != 'IUPAC Name':
+    if urn_label.text != label:
         return
     
-    urn_name = urn.find('PC-Urn_name')
-    if urn_name is None:
-        return
-    if urn_name.text != 'Allowed':
-        return
-    
-    data_value = node.find('PC-InfoData_value')
-    if data_value is None:
-        return
-    sval = data_value.find('PC-InfoData_value_sval')
-    if sval is None:
-        return
-    return sval.text
-
-def get_inchi_key(node):
-    if node.tag != 'PC-InfoData':
-        return
-    
-    data_urn = node.find('PC-InfoData_urn')
-    if data_urn is None:
-        return
-    urn = data_urn.find('PC-Urn')
-    if urn is None:
-        return
-    urn_label = urn.find('PC-Urn_label')
-    if urn_label is None:
-        return
-    if urn_label.text != 'InChIKey':
-        return
-    
-    urn_name = urn.find('PC-Urn_name')
-    if urn_name is None:
-        return
-    if urn_name.text != 'Standard':
-        return
+    if name != None:
+        urn_name = urn.find('PC-Urn_name')
+        if urn_name is None:
+            return
+        if urn_name.text != name:
+            return
     
     data_value = node.find('PC-InfoData_value')
     if data_value is None:
         return
-    sval = data_value.find('PC-InfoData_value_sval')
-    if sval is None:
+    val = data_value.find(f'PC-InfoData_value_{type_}')
+    if val is None:
         return
-    return sval.text
+    return val.text
 
 def parse_xml(in_filepath, out_folderpath):
 
@@ -73,20 +44,86 @@ def parse_xml(in_filepath, out_folderpath):
         if '}' in node.tag:
             node.tag = node.tag.split('}')[-1]
         
+        if node.tag == 'PC-Compound':
+            node_list.append(node_data)
+            node_data = {}
+
+            # to-do: remove this
+            if len(node_list) >= 2:
+                break
 
         if node.tag == 'PC-CompoundType_id_cid':
-            if len(node_data) != 0:
-                node_list.append(node_data)
-            node_data = {}
             node_data['cid'] = node.text
 
-        res = get_iupac(node)
+        if node.tag == 'PC-Compound_charge':
+            node_data['charge'] = node.text
+
+        res = get_infodata(node, 'Compound Complexity', type_='fval')
+        if res != None:
+            node_data['compound_complexity'] = res
+
+        res = get_infodata(node, 'Count', name='Hydrogen Bond Acceptor', type_='ival')
+        if res != None:
+            node_data['count_hydrogen_bond_acceptor'] = res
+
+        res = get_infodata(node, 'Count', name='Hydrogen Bond Donor', type_='ival')
+        if res != None:
+            node_data['count_hydrogen_bond_donor'] = res
+
+        res = get_infodata(node, 'Count', name='Rotatable Bond', type_='ival')
+        if res != None:
+            node_data['count_rotatable_bond'] = res
+
+        res = get_infodata(node, 'Fingerprint', name='SubStructure Keys', type_='binary')
+        if res != None:
+            node_data['fingerprint_substructurekeys'] = res
+        
+        res = get_infodata(node, 'IUPAC Name', name='Allowed')
         if res != None:
             node_data['iupac'] = res
         
-        res = get_inchi_key(node)
+        res = get_infodata(node, 'InChI', name='Standard')
+        if res != None:
+            node_data['inchi'] = res
+        
+        res = get_infodata(node, 'InChIKey', name='Standard')
         if res != None:
             node_data['inchi_key'] = res
+
+        res = get_infodata(node, 'Log P', type_='fval')
+        if res != None:
+            node_data['log_p'] = res
+
+        res = get_infodata(node, 'Mass')
+        if res != None:
+            node_data['mass'] = res
+
+        res = get_infodata(node, 'Molecular Formula')
+        if res != None:
+            node_data['molecular_formula'] = res
+
+        res = get_infodata(node, 'Molecular Weight')
+        if res != None:
+            node_data['molecular_weight'] = res
+
+        res = get_infodata(node, 'SMILES', name='Canonical')
+        if res != None:
+            node_data['smiles_canonical'] = res
+
+        res = get_infodata(node, 'SMILES', name='Isomeric')
+        if res != None:
+            node_data['smiles_isomeric'] = res
+
+        res = get_infodata(node, 'Topological', name='Polar Surface Area', type_='fval')
+        if res != None:
+            node_data['polar_surface_area'] = res
+
+        res = get_infodata(node, 'Weight', name='MonoIsotopic')
+        if res != None:
+            node_data['weight_monoisotopic'] = res
+
+        
+            
 
     if len(node_data) != 0:
         node_list.append(node_data)
