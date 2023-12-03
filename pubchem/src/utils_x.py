6d5ab2f9c75f302cdcf0f90b849b8da3d8a0289c
Saketh <saketh9977@gmail.com>
2023-12-03 17:13:00 +0530

    added csv_to_json flat (not for nested) function

--- pubchem/src/utils_x.py
+++ pubchem/src/utils_x.py
@@ -2,6 +2,7 @@ import json
 import os, shutil
 from datetime import datetime, timedelta
 import ftplib
+import csv
 
 def print_x(data):
     timestamp_str = (datetime.utcnow() + timedelta(hours=5, minutes=30)).strftime('%Y-%m-%d %H:%M:%S IST')
@@ -20,6 +21,10 @@ def write_to_json(data_list, out_filepath):
 
 def list_ftp_files(ftp_url):
 
+    """
+        example: "https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/CURRENT-Full/XML"
+    """
+
     out = []
     parts = ftp_url.split("/")
     server = parts[2]
@@ -35,7 +40,30 @@ def list_ftp_files(ftp_url):
 
     return out
 
+def get_csv_from_json(in_json_path, out_csv_path):
+
+    csv_2d_list = []
+    header_row = []
+    with open(in_json_path, 'r') as in_stream:
+        for ind, line in enumerate(in_stream):
+            if line.strip() == '':
+                continue
+            line_dict = json.loads(line)
+
+            if ind == 0:
+                for key in line_dict:
+                    header_row.append(key)
+
+            row = []
+            for key in header_row:
+                row.append(line_dict[key])
+            csv_2d_list.append(row)
+
+    with open(out_csv_path, 'w+') as out_stream:
+        csv_writer = csv.writer(out_stream)
+        csv_writer.writerows(csv_2d_list)
 
 if __name__ == '__main__':
-    ftp_url = "https://ftp.ncbi.nlm.nih.gov/pubchem/Compound/CURRENT-Full/XML"
-    list_ftp_files(ftp_url)
+    in_json_path = '../out/2.json'
+    out_csv_path = '../out/2.csv'
+    get_csv_from_json(in_json_path, out_csv_path)
